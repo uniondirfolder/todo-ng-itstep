394ef0795f66d7759a3462609c37a7dcf3175be2
NVV <uniondirfolder@gmail.com>
2021-05-11 12:25:55 +0300

    add components struct

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './components/todo-item/item/item.component';
+import { ListComponent } from './components/todos/list/list.component';
+import { HeaderComponent } from './components/pages/header/header.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { AddComponent } from './components/add-todo/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ListComponent,
+    HeaderComponent,
+    AboutComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
